96667cdd3da106e1619edcf1822d7fabe07cf16c
wenzhaoabc <wenzhaoabc@gmail.com>
2022-08-06 12:43:05 +0800

    finish lab thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET];
 
 double
 now()
@@ -40,6 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  
 
   // is the key already present?
   struct entry *e = 0;
@@ -47,6 +49,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(locks+i);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,7 +57,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(locks+i);
 }
 
 static struct entry*
@@ -105,6 +108,8 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  for(int i = 0;i < NBUCKET;i++)
+    pthread_mutex_init(locks+i,NULL);
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
